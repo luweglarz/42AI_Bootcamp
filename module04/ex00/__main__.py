d02905662940b32f373039fbae94ea3d4d74c17a
RolandGamos <lucasweglarz9@gmail.com>
2021-11-22 14:43:00 +0100

    Module04 ex00 done

---
+++ module04/ex00/__main__.py
@@ -0,0 +1,11 @@
+from FileLoader import FileLoader
+
+def main():
+    Fl = FileLoader()
+
+    load = Fl.load("./athlete_events.csv")
+    Fl.display(load, 6)
+
+if __name__ == "__main__":
+    print(__name__)
+    main()
