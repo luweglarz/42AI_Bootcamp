5ee920338241f5e5592320cff3e90da8bdf6eb5c
Lucas WEGLARZ <lweglarz@e3r7p7.42.fr>
2021-11-23 15:23:16 +0100

    Module04 ex06 done

--- module04/ex06/MyPlotLib.py
+++ module04/ex06/MyPlotLib.py
@@ -1,19 +1,38 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import seaborn as sb
+
 class MyPlotLib:
 
     @staticmethod
     def histogram(data, features):
-        fig, ax = plt.subplots(1, len(features))
-        uniq = data.drop_duplicates(['Name'])
+        fig, ax = plt.subplots(len(features))
+        NoDuplicates = data.drop_duplicates(['Name'])
         i = 0
         for feature in features:
-            print(np.object)
-            df = uniq[feature]
+            df = data[feature]
             ax[i].set_title(feature)
             ax[i].hist(df)
-            ax[i].grid()
             i += 1
         plt.show()
 
+    @staticmethod
+    def density(data, features):
+        NoDuplicates = data.drop_duplicates(['Name'])
+        ax = sb.kdeplot(data=NoDuplicates[features], 
+        fill=True, common_norm=False, palette="crest", 
+        alpha=.5, linewidth=0) 
+        plt.show()
+
+    @staticmethod
+    def pair_plot(data, features):
+        NoDuplicates = data.drop_duplicates(['Name'])
+        ax = sb.pairplot(data=NoDuplicates[features], diag_kind="hist") 
+        plt.show()
+
+    @staticmethod
+    def box_plot(data, features):
+        NoDuplicates = data.drop_duplicates(['Name'])
+        sb.boxplot(data=NoDuplicates[features])
+        plt.show()
--- module04/ex06/__main__.py
+++ module04/ex06/__main__.py
@@ -6,7 +6,7 @@ def main():
     load = Fl.load("./athlete_events.csv")
 
     mpl = MyPlotLib()
-    mpl.histogram(load, ['Weight', 'Height'])
+    mpl.box_plot(load, ['Weight', 'Height'])
 
 if __name__ == "__main__":
     main()
